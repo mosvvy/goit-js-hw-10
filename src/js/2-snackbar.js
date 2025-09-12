266fbdd13d8478ca83268ee5d1b8f97a22ac4d15
ValeriiaM <moskalenkovvy@gmail.com>
2025-09-12 22:36:21 +0300

    t2 | add logic code

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -14,5 +14,44 @@ import iziToast from 'izitoast';
 // Додатковий імпорт стилів
 import 'izitoast/dist/css/iziToast.min.css';
 
-const successMsg = `✅ Fulfilled promise in ${delay}ms`;
-const failMsg = `❌ Rejected promise in ${delay}ms`;
+const form = document.querySelector('form');
+
+const promices = [];
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const delay = event.target.elements.delay.value;
+  const state = event.target.elements.state.value;
+
+  console.log(delay);
+  console.log(state);
+
+  promices.push(
+    new Promise((resolve, reject) => {
+      if (state == 'fulfilled') {
+        setTimeout(() => {
+          iziToast.show({
+            message: `✅ Fulfilled promise in ${delay}ms`,
+            position: 'topRight',
+            backgroundColor: '#59A10D',
+            messageColor: '#FFFFFF',
+            progressBarColor: '#326101',
+            overlayColor: '#B5EA7C',
+          });
+        }, delay);
+      } else {
+        setTimeout(() => {
+          iziToast.show({
+            message: `❌ Rejected promise in ${delay}ms`,
+            position: 'topRight',
+            backgroundColor: '#EF4040',
+            messageColor: '#FFFFFF',
+            progressBarColor: '#B51B1B',
+            overlayColor: '#FFBEBE',
+          });
+        }, delay);
+      }
+    })
+  );
+});
