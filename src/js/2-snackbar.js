9c9f681d49a64dc6aa9c2629a57b7e45edbb64e7
ValeriiaM <moskalenkovvy@gmail.com>
2025-09-13 15:59:08 +0300

    mowe Timeoun to Promice creation. Number the delay. remove promices array

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -5,48 +5,39 @@ import 'izitoast/dist/css/iziToast.min.css';
 
 const form = document.querySelector('form');
 
-const promices = [];
-
 form.addEventListener('submit', event => {
   event.preventDefault();
 
   const delay = event.target.elements.delay.value;
   const state = event.target.elements.state.value;
 
-  console.log(delay);
-  console.log(state);
-
-  promices.push(
-    new Promise((resolve, reject) => {
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
       if (state == 'fulfilled') {
         resolve(delay);
       } else {
         reject(delay);
       }
+    }, Number(delay));
+  })
+    .then(delay => {
+      iziToast.show({
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+        backgroundColor: '#59A10D',
+        messageColor: '#FFFFFF',
+        progressBarColor: '#326101',
+        overlayColor: '#B5EA7C',
+      });
     })
-      .then(delay => {
-        setTimeout(() => {
-          iziToast.show({
-            message: `✅ Fulfilled promise in ${delay}ms`,
-            position: 'topRight',
-            backgroundColor: '#59A10D',
-            messageColor: '#FFFFFF',
-            progressBarColor: '#326101',
-            overlayColor: '#B5EA7C',
-          });
-        }, delay);
-      })
-      .catch(delay => {
-        setTimeout(() => {
-          iziToast.show({
-            message: `❌ Rejected promise in ${delay}ms`,
-            position: 'topRight',
-            backgroundColor: '#EF4040',
-            messageColor: '#FFFFFF',
-            progressBarColor: '#B51B1B',
-            overlayColor: '#FFBEBE',
-          });
-        }, delay);
-      })
-  );
+    .catch(delay => {
+      iziToast.show({
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topRight',
+        backgroundColor: '#EF4040',
+        messageColor: '#FFFFFF',
+        progressBarColor: '#B51B1B',
+        overlayColor: '#FFBEBE',
+      });
+    });
 });
